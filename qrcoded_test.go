1d49abc977820601030bd2bbec4406867f48312a
Apipol Sukgler <apipol@computerlogy.com>
2018-04-12 13:41:02 +0700

    STEP 11: add ErrorWriter stub to make error to simulate test that expect error, test will FAILED

--- qrcoded_test.go
+++ qrcoded_test.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"bytes"
-	"image/png"
+	"errors"
 	"testing"
 )
 
-func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
-	buffer := new(bytes.Buffer)
-	GenerateQRCode(buffer, "555-2368")
-	_, err := png.Decode(buffer)
+type ErrorWriter struct{}
 
-	if err != nil {
+func (e *ErrorWriter) Write(b []byte) (int, error) {
+	return 0, errors.New("Expected error")
+}
+
+func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
+	w := new(ErrorWriter)
+	err := GenerateQRCode(w, "555-2368")
+
+	if err == nil || err.Error() != "Expected error" {
 		t.Errorf("Generated QRCode is not a PNG: %s", err)
 	}
 }
