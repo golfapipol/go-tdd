44229b9fc08ddd0e27d6d70e7ad455f20a4bd000
Apipol Sukgler <apipol@computerlogy.com>
2018-04-12 11:32:48 +0700

    STEP 7: Make GeneratesPNG PASS

--- qrcoded.go
+++ qrcoded.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image"
+	"image/png"
 	"io/ioutil"
 )
 
 func GenerateQRCode(code string) []byte {
-	return []byte{0xFF}
+	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
+	buf := new(bytes.Buffer)
+	_ = png.Encode(buf, img)
+	return buf.Bytes()
 }
 
 func main() {
