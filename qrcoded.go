df764e2f32d1760a26b9b74474d5c57c6016e076
Apipol Sukgler <apipol@computerlogy.com>
2018-04-12 11:06:46 +0700

    STEP 2: add call GenerateQRCode and writeFile by using ioutil (but it will not compile

--- qrcoded.go
+++ qrcoded.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	fmt.Println("Hello QR Code")
+
+	qrcode := GenerateQRCode("555-2368")
+	ioutil.WriteFile("qrcode.png", qrcode, 0644)
 }
